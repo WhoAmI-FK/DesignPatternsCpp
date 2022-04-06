ab902f4fea2f9b2f1db05bc633115b00b3b39c60
WhoAmI-FK <Faismen2004@mail.ru>
2022-04-06 19:28:53 +0200

    first steps

---
+++ Design Patterns.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+
+namespace whoamifk {
+    class User {
+    private:
+        std::string _name;
+        std::string _email;
+        std::string _password;
+    public:
+        explicit User() = default;
+
+    };
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
