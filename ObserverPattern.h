82f544de91c8c2e19932a79578e0357f9fa172cd
WhoAmI-FK <Faismen2004@mail.ru>
2022-04-08 19:31:34 +0200

    ObserverPattern set

---
+++ ObserverPattern.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <vector>
+namespace observers {
+	class Subscriber {
+	public:
+		virtual void notify(const std::string& publisherName, const std::string& message) = 0;
+		virtual std::string getName();
+	};
+	class Publisher {
+	public:
+		virtual void subscribe(Subscriber* sub) = 0;
+		virtual void unsubscribe(Subscriber* sub) = 0;
+		virtual void publish(const std::string& message) = 0;
+	};
+	class ChatGroup : public Publisher {
+	private:
+		std::string groupName;
+		std::vector<Subscriber*> subs;
+	public:
+		ChatGroup(const std::string& name) :
+			groupName(name) {
+
+		};
+		void subscribe(Subscriber* sub) override {
+			this->subs.push_back(sub);
+		};
+		void unsubscribe(Subscriber* sub) override {
+			this->subs.erase(std::remove_if(this->subs.begin(), this->subs.end(),
+				[sub](Subscriber* s) { return s->getName() == sub->getName(); }), this->subs.end());
+		};
+		void publish(const std::string& message) override {
+			for (auto sub : this->subs) {
+				sub->notify(groupName, message);
+			}
+		};
+	};
+};
